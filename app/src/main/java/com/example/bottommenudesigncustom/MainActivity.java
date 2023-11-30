bf960d8f49b2c9cf427763228d2e204083efde15
DELL <hominphuzxc@gmail.com>
2023-11-30 11:42:57 +0700

    Config app

---
+++ app/src/main/java/com/example/bottommenudesigncustom/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.bottommenudesigncustom;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.bottommenudesigncustom.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+}
